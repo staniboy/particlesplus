94d530a2e9dd300896c9e4152fb580d502b3b113
Staniboy <stanley.v@outlook.com>
2025-07-29 17:05:06 +0400

    refactor: moved config classes

---
+++ ParticlesPlus/ParticlesPlusConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Vintagestory.API.Common;
+
+namespace ParticlesPlus
+{
+    public class ModConfig
+    {
+        public int Version { get; set; }
+        public bool Global { get; set; } = true;
+        public Dictionary<string, PresetConfig> Presets { get; set; } = new();
+        public Dictionary<string, AdvancedParticleProperties[]> Particles { get; set; } = new();
+    }
+
+    public class PresetConfig
+    {
+        public bool Enabled { get; set; }
+        public string Wildcard { get; set; }
+        public string Particles { get; set; }
+    }
+}
--- ParticlesPlus/ParticlesPlusModSystem.cs
+++ ParticlesPlus/ParticlesPlusModSystem.cs
@@ -7,20 +7,6 @@ using System.Linq;
 
 namespace ParticlesPlus
 {
-    public class ModConfig
-    {
-        public int Version { get; set; }
-        public bool Global { get; set; } = true;
-        public Dictionary<string, PresetConfig> Presets { get; set; } = new();
-        public Dictionary<string, AdvancedParticleProperties[]> Particles { get; set; } = new();
-    }
-
-    public class PresetConfig
-    {
-        public bool Enabled { get; set; }
-        public string Wildcard { get; set; }
-        public string Particles { get; set; }
-    }
     public class ParticlesPlusModSystem : ModSystem
     {
         public static ModConfig LoadedConfig { get; private set; }
