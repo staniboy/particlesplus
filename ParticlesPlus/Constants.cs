fc79fbcb2962c7b36c228c1a4d2604ce0d5761c1
Staniboy <stanley.v@outlook.com>
2025-08-01 17:10:56 +0400

    refactor: removed "Error" from error messages

--- ParticlesPlus/Constants.cs
+++ ParticlesPlus/Constants.cs
@@ -7,9 +7,9 @@
             public const string PresetAdded = "New preset added!";
             public const string PresetSaved = "Preset changes saved!";
             public const string PresetRemoved = "Preset removed!";
-            public const string RegexError = "Error: Invalid regex!";
-            public const string EmptyNameError = "Error: Name cannot be empty!";
-            public const string DuplicateNameError = "Error: Preset with this name already exists!";
+            public const string RegexError = "Invalid regex!";
+            public const string EmptyNameError = "Name cannot be empty!";
+            public const string DuplicateNameError = "Preset with this name already exists!";
         }
     }
 }
