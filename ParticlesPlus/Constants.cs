758443247f8edb843712a1e121cf51efe3ff954e
Staniboy <stanley.v@outlook.com>
2025-08-01 16:56:09 +0400

    feat: Add static class for constants

---
+++ ParticlesPlus/Constants.cs
@@ -0,0 +1,15 @@
+﻿namespace ParticlesPlus
+{
+    internal class Constants
+    {
+        public static class ChatMessages
+        {
+            public const string PresetAdded = "New preset added!";
+            public const string PresetSaved = "Preset changes saved!";
+            public const string PresetRemoved = "Preset removed!";
+            public const string RegexError = "Error: Invalid regex!";
+            public const string EmptyNameError = "Error: Name cannot be empty!";
+            public const string DuplicateNameError = "Error: Preset with this name already exists!";
+        }
+    }
+}
